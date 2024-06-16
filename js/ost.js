7e343416990f1bf9027b145d014c545a8371708a
배주연 <w2316@e-mirim.hs.kr>
2024-06-16 15:07:19 +0900

    fix : 노래 추가

--- js/ost.js
+++ js/ost.js
@@ -1,32 +1,43 @@
-//audioPlayer : 재생/일시정지 하기 위해서
 const audioPlayer = document.getElementById('audio-player');
 
 const tracks = {
     ost1: 'audio/아무것도아니야.mp3',
     ost2: 'audio/Orange.mp3',
     ost3: 'audio/츠루네.mp3',
-    ost4: 'audio/킥백.mp3'
+    ost4: 'audio/킥백.mp3',
+    oo1: 'audio/세계는사랑에빠져있어.mp3',
+    oo2: 'audio/너에게 닿기를.mp3',
+    oo3: 'audio/신령님.mp3',
+    oo4: 'audio/변하지않는것들.mp3',
+    oo5: 'audio/문단속.mp3',
+    oo6: 'audio/바람이되어.mp3',
+    oo7: 'audio/Tiny Light.mp3',
+    oo8: 'audio/거짓말쟁이.mp3',
+    oo9: 'audio/이정표.mp3'
 };
 
-let currentTrack = '';  //현재 재생 중인 특랙의 key값을 저장하는 변수 
+let currentTrack = '';  // 현재 재생 중인 트랙의 key값을 저장하는 변수 
 
-document.querySelectorAll('.Ost img').forEach(item => {
+document.querySelectorAll('.Ost img, .oo').forEach(item => {
     item.addEventListener('click', event => {
-        const trackId = event.target.id;
+        let trackId = event.target.id || event.target.querySelector('img')?.id;
+        if (item.classList.contains('oo')) {
+            trackId = item.id;
+        }
+
         const trackSrc = tracks[trackId];
 
         if (currentTrack === trackId) {
-            
-            if (audioPlayer.paused) { //paused = true : 일시 정지 상태임
+            if (audioPlayer.paused) {
                 audioPlayer.play();
             } else {
                 audioPlayer.pause();
             }
         } else {
-            
             audioPlayer.src = trackSrc;
             audioPlayer.play();
             currentTrack = trackId;
         }
+ 
     });
 });
