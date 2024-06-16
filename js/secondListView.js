22bdc94628f21da9b58cc87c08555c03ff94fbd3
배주연 <w2316@e-mirim.hs.kr>
2024-06-16 15:12:01 +0900

    fix : 검색시 해당 페이지 넘어가도록 수정

--- js/secondListView.js
+++ js/secondListView.js
@@ -1,4 +1,12 @@
 const movies = {
+    "귀멸의 칼날": "info.html",
+    "목소리의 형태": "info1.html",
+    "하이큐!! 쓰레기장의 결전": "info2.html",
+    "너의 이름은": "info3.html",
+    "스즈메의 문단속": "info4.html",
+    "시간을 달리는 소녀": "info5.html",
+    "우리의 계절은": "info6.html",
+    "주술회전": "info7.html",
     "인사이드아웃2": "info2-1.html",
     "업": "info2-2.html",
     "라푼젤": "info2-3.html",
@@ -6,7 +14,15 @@ const movies = {
     "코코": "info2-5.html",
     "카3": "info2-6.html",
     "주먹왕 랄프": "info2-7.html",
-    "칩과 데일:다람쥐 구조대": "info2-8.html"
+    "칩과 데일:다람쥐 구조대": "info2-8.html",
+    "엘리멘탈": "info3-1.html",
+    "모아나": "info3-2.html",
+    "빅 히어로": "info3-3.html",
+    "인어공주": "info3-4.html",
+    "위시": "info3-5.html",
+    "알라딘": "info3-6.html",
+    "뮬란": "info3-7.html",
+    "메리다와 마법의 숲": "info3-8.html"
  }
 
  function submitForm(event) {
